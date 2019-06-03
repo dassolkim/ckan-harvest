dbafeaeba4ec0300d4796a4c86f70ecec66758e3
dassolkim <kimds0926@gmail.com>
2019-06-03 14:53:42 +0900

    add new source delete option to template

--- ckanext/harvest/logic/action/delete.py
+++ ckanext/harvest/logic/action/delete.py
@@ -47,7 +47,8 @@ def harvest_source_delete(context, data_dict):
 
     # p.toolkit.get_action('harvest_source_job_history_clear')(
     #     context, {'id': package_dict['id']})
-    log.info('finish harvest source history clear')
+    
+    # log.info('finish harvest source history clear')
 
     # model = context['model']
     # sql = '''begin;
@@ -63,17 +64,79 @@ def harvest_source_rel_info_delete(context, data_dict):
     harvest_source_id = data_dict['id']
     # source_delete = data_dict['source_delete']
     model = context['model']
+
+    # harvest_source_index_clear
+
     sql = '''begin;
-            delete from harvest_object_error where harvest_object_id
-             in (select id from harvest_object where harvest_source_id = '{harvest_source_id}');
-            delete from harvest_object_extra where harvest_object_id
-             in (select id from harvest_object where harvest_source_id = '{harvest_source_id}');
-            delete from harvest_object where harvest_source_id = '{harvest_source_id}';
-            delete from harvest_gather_error where harvest_job_id
-             in (select id from harvest_job where source_id = '{harvest_source_id}');
-            delete from harvest_job where source_id = '{harvest_source_id}';
-            commit;
-            '''.format(harvest_source_id=harvest_source_id)
+            update package set state = 'to_delete' where id in (
+                select package_id from harvest_object
+                where harvest_source_id = '{harvest_source_id}');'''.format(
+        harvest_source_id=harvest_source_id)
+
+    sql += '''
+    delete from resource_view where resource_id in (
+        select id from resource where package_id in (
+            select id from package where state = 'to_delete'));
+    delete from resource_revision where package_id in (
+        select id from package where state = 'to_delete');
+    delete from resource where package_id in (
+        select id from package where state = 'to_delete');
+        '''
+    sql += '''
+        delete from harvest_object_error where harvest_object_id in (
+            select id from harvest_object
+            where harvest_source_id = '{harvest_source_id}');
+        delete from harvest_object_extra where harvest_object_id in (
+            select id from harvest_object
+            where harvest_source_id = '{harvest_source_id}');
+        delete from harvest_object where harvest_source_id = '{harvest_source_id}';
+        delete from harvest_gather_error where harvest_job_id in (
+            select id from harvest_job where source_id = '{harvest_source_id}');
+        delete from harvest_job where source_id = '{harvest_source_id}';
+        delete from package_tag_revision where package_id in (
+            select id from package where state = 'to_delete');
+        delete from member_revision where table_id in (
+            select id from package where state = 'to_delete');
+        delete from package_extra_revision where package_id in (
+            select id from package where state = 'to_delete');
+        delete from package_revision where id in (
+            select id from package where state = 'to_delete');
+        delete from package_tag where package_id in (
+            select id from package where state = 'to_delete');
+        delete from package_extra where package_id in (
+            select id from package where state = 'to_delete');
+        delete from package_relationship_revision where subject_package_id in (
+            select id from package where state = 'to_delete');
+        delete from package_relationship_revision where object_package_id in (
+            select id from package where state = 'to_delete');
+        delete from package_relationship where subject_package_id in (
+            select id from package where state = 'to_delete');
+        delete from package_relationship where object_package_id in (
+            select id from package where state = 'to_delete');
+        delete from member where table_id in (
+            select id from package where state = 'to_delete');
+         '''.format(
+        harvest_source_id=harvest_source_id)
+    sql += '''
+            delete from package where id in (
+                select id from package where state = 'to_delete');
+            '''
+
+    # sql += '''begin;
+    #         delete from harvest_object_error where harvest_object_id
+    #          in (select id from harvest_object where harvest_source_id = '{harvest_source_id}');
+    #         delete from harvest_object_extra where harvest_object_id
+    #          in (select id from harvest_object where harvest_source_id = '{harvest_source_id}');
+    #         delete from harvest_object where harvest_source_id = '{harvest_source_id}';
+    #         delete from harvest_gather_error where harvest_job_id
+    #          in (select id from harvest_job where source_id = '{harvest_source_id}');
+    #         delete from harvest_job where source_id = '{harvest_source_id}';
+    #         commit;
+    #         '''.format(harvest_source_id=harvest_source_id)
+    sql += '''
+        commit;
+        '''
+    # model.Session.execute(sql)
 
     model.Session.execute(sql)
 
@@ -82,7 +145,7 @@ def harvest_source_rel_info_delete(context, data_dict):
 
     log.info('finish harvest source history clear')
     print('############################### check harvest_source_rel_info_delete data_dict line 93 ##################################')
-
+    # harvest_source delete
     sql = '''begin;
             delete from harvest_source where id='{harvest_source_id}';
             commit;
